c0b0abb00207684925055e2a4e0334f033bcd20c
dev <mrc@pop-os.localdomain>
2025-07-02 01:32:39 +0300

    Integrate Firebase into the Flutter project by adding firebase_core and firebase_auth dependencies. Update Android build configuration with Google services plugin and include google-services.json. Adjust local.properties and Flutter environment paths for consistency. Add GoogleService-Info.plist for iOS configuration. Modify main.dart to initialize Firebase on app startup.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/dev/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/dev/Documents/projects/tabip/tabip"
+export "FLUTTER_ROOT=/home/mrc/Documents/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/mrc/Documents/projects/Tabip"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
